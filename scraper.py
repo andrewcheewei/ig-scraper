ee0982feebd3f227fa8bc8c132199110e4b89b1d
Andrew Chee <andrewcheewei@gmail.com>
2024-07-22 01:55:43 -0400

    added setup file

--- scraper.py
+++ scraper.py
@@ -14,6 +14,8 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#!/usr/bin/env python3
+
 import getpass
 from datetime import datetime
 import requests_html
