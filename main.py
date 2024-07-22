a32e60e89f5fb3a7f7d7e2ec299521e3b9c81d36
Andrew Chee <andrewcheewei@gmail.com>
2024-07-22 01:55:34 -0400

    add setup file

--- main.py
+++ main.py
@@ -14,6 +14,8 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#!/usr/bin/env python3
+
 from scraper import *
 
 scrape()
